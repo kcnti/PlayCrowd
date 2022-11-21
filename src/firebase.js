d7f8d1c93af5d8eaf92484b0f275a14b3224b026
kcnti <38667172+kcnti@users.noreply.github.com>
2022-11-21 21:25:49 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyB4Mp_pY-4R9_bJJk_oyMA_mPdEUQf5dXg",
+    apiKey: "-",
     databaseURL: "https://playcrowd-571fb-default-rtdb.asia-southeast1.firebasedatabase.app/",
     authDomain: "playcrowd-571fb.firebaseapp.com",
     projectId: "playcrowd-571fb",
     storageBucket: "playcrowd-571fb.appspot.com",
-    messagingSenderId: "712603493825",
-    appId: "1:712603493825:web:6e0d5fd68be0c34048fb40",
-    measurementId: "G-MTV0D082EY"
+    messagingSenderId: "-",
+    appId: "-",
+    measurementId: "-"
 });
 
 export const auth = app.auth();
